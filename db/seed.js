cb736d7372b8cc642e42a81163c463435d1aec63
Zhijie Xu <jennyxu000@gmail.com>
2020-07-20 16:21:43 -0700

    seed test

--- db/seed.js
+++ db/seed.js
@@ -5,64 +5,64 @@ const Stop = require("../models/stop");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const main = () => {
-  Trip.find({}).deleteMany(() => {
-    Stop.find({}).deleteMany(() => {
-      let trip1 = Trip.create({
-        name: "Summer Trip",
-        packingList: ["coat", "gloves", "heater"],
-        departureDate: "2019-8-13",
-      }).then((trip) => {
-        Promise.all([
-          Stop.create({
-            name: "North Pole",
-            lat: 30,
-            lng: 40,
-            thingsToDo: ["ride a sled", "touch a Husky", "swim in ice sea"],
-          }).then((createdStop) => {
-            trip.stops.push(createdStop);
-          }),
-          Stop.create({
-            name: "Alaska",
-            lat: 40,
-            lng: -190,
-            thingsToDo: ["live in ice house", "catch a fish"],
-          }).then((createdStop) => {
-            trip.stops.push(createdStop);
-          }),
-        ]).then(() => {
-          trip.save();
-        });
+// const main = () => {
+Trip.find({}).deleteMany(() => {
+  Stop.find({}).deleteMany(() => {
+    let trip1 = Trip.create({
+      name: "Summer Trip",
+      packingList: ["coat", "gloves", "heater"],
+      departureDate: "2019-8-13",
+    }).then((trip) => {
+      Promise.all([
+        Stop.create({
+          name: "North Pole",
+          lat: 30,
+          lng: 40,
+          thingsToDo: ["ride a sled", "touch a Husky", "swim in ice sea"],
+        }).then((createdStop) => {
+          trip.stops.push(createdStop);
+        }),
+        Stop.create({
+          name: "Alaska",
+          lat: 40,
+          lng: -190,
+          thingsToDo: ["live in ice house", "catch a fish"],
+        }).then((createdStop) => {
+          trip.stops.push(createdStop);
+        }),
+      ]).then(() => {
+        trip.save();
       });
-      let trip2 = Trip.create({
-        name: "Winter Trip",
-        packingList: ["water", "sunscreen", "blanket"],
-        departureDate: "2019-12-13",
-      }).then((trip) => {
-        Promise.all([
-          Stop.create({
-            name: "North Pole",
-            lat: 30,
-            lng: 40,
-            thingsToDo: ["ride a sled", "touch a Husky", "swim in ice sea"],
-          }).then((createdStop) => {
-            trip.stops.push(createdStop);
-          }),
-          Stop.create({
-            name: "Alaska",
-            lat: 40,
-            lng: -190,
-            thingsToDo: ["live in ice house", "catch a fish"],
-          }).then((createdStop) => {
-            trip.stops.push(createdStop);
-          }),
-        ]).then(() => {
-          trip.save();
-        });
+    });
+    let trip2 = Trip.create({
+      name: "Winter Trip",
+      packingList: ["water", "sunscreen", "blanket"],
+      departureDate: "2019-12-13",
+    }).then((trip) => {
+      Promise.all([
+        Stop.create({
+          name: "North Pole",
+          lat: 30,
+          lng: 40,
+          thingsToDo: ["ride a sled", "touch a Husky", "swim in ice sea"],
+        }).then((createdStop) => {
+          trip.stops.push(createdStop);
+        }),
+        Stop.create({
+          name: "Alaska",
+          lat: 40,
+          lng: -190,
+          thingsToDo: ["live in ice house", "catch a fish"],
+        }).then((createdStop) => {
+          trip.stops.push(createdStop);
+        }),
+      ]).then(() => {
+        trip.save();
       });
     });
   });
-};
+});
+// };
 
 // const run = async () => {
 //   await main();
@@ -71,4 +71,4 @@ const main = () => {
 
 // run();
 
-main();
+// main();
