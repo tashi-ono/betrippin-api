33d00498afc1aed10c3232cd19e9b5d9ad87fc86
Zhijie Xu <jennyxu000@gmail.com>
2020-07-20 13:20:38 -0700

    packing list test

--- models/trip.js
+++ models/trip.js
@@ -3,7 +3,7 @@ const mongoose = require("../db/connection");
 const tripSchema = new mongoose.Schema({
   name: { type: String, required: true },
   // stops: [{ type: mongoose.Schema.Types.ObjectId, ref: "stop" }],
-  // packingList: [{ type: String }],
+  packingList: [{ type: String }],
   departureDate: { type: Date },
 });
 
